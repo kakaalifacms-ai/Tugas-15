6cf1df3ad336ad91d4ce899aa36fbec4eeeeb362
kaka alifha reza <kakaalifacms@gmail.com>
2025-11-11 10:24:12 +0700

    update

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-console.log('Hello World!');
+function CekNilai() {
+  // Mengambil value dari elemen input nilai
+  // Simpan ke dalam vartabel bernama "Nilai"
+ let Nilai = Number(document.getElementById("Nilai").value)
+  
+  // menginisiasi variabel "hasil" isinya string kosong""
+  let hasil = ""
+  
+  // menentukan angka A B C D berdasarkan isi variabel "Nilai"
+  
+  // menggunakan percabangan if (jika)
+  
+ // dan membandingkan variabel "Nilai"
+  if (Nilai>= 88) {
+   hasil  = "Nilai A (Sangat Baik) "
+  }else if (Nilai >=75) {
+   hasil= "Nilai B (Baik) "
+  }else if(Nilai >=51) {
+   hasil= "Nilai C (Cukup Baik) "
+}
+
+// tampilkan isi Vartebel "hasil ke elemen " hasil" di html
+document.getElementById('hasil').innerText=hasil
+
+}
